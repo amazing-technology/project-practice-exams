a4191f34a5c79639135e9f3ebad4897d757094ff
sentongo@sentongo <sentongoalbert12@gmail.com>
2022-10-24 02:04:48 +0200

    I am adding all table

---
+++ practice/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+from import_export.admin import ImportExportModelAdmin
+
+# Register your models here.
+admin.site.register(Exams)
+admin.site.register(Marks)
+admin.site.register(Question)
+admin.site.register(Set)
+admin.site.register(Answers)
+admin.site.register(Candidates)
+admin.site.register(Users)
+# admin.site.register( Quastion_db)
+
+@admin.register(Quastion_db)
+class Quastion_db(ImportExportModelAdmin):
+    list_display = ('questionName', 'optionA', 'optionB', 'optionC', 'optionD', 'answer')
